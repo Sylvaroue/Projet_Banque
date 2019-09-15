c721b03fd14e00bfb2e0429aa2524d1fbb7b542d
Syl20 <sylvain.rou5@gmail.com>
2019-09-15 19:41:32 +0200

    Master à jour

--- src/main/java/com/inti/entities/Client.java
+++ src/main/java/com/inti/entities/Client.java
@@ -3,9 +3,6 @@ package com.inti.entities;
 import java.util.Set;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
